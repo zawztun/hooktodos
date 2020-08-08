2d9d276fa4829474f3b4fbf984327c6e5d069a55
zawtun <zzt.nyc@gmail.com>
2020-08-08 10:53:17 -0400

    Updates

---
+++ precache-manifest.bf26354e45bd7213878db45e9c0220f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83414dbf4c5aaea894f4fd5bd4971ce",
+    "url": "/hooktodos/index.html"
+  },
+  {
+    "revision": "d8d01a313b1386f6edf5",
+    "url": "/hooktodos/static/css/main.737ea37c.chunk.css"
+  },
+  {
+    "revision": "43fe43625d4714333576",
+    "url": "/hooktodos/static/js/2.4b9819c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hooktodos/static/js/2.4b9819c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d01a313b1386f6edf5",
+    "url": "/hooktodos/static/js/main.26a7a138.chunk.js"
+  },
+  {
+    "revision": "3795d5ff3aed5e00ac78",
+    "url": "/hooktodos/static/js/runtime-main.fadc66af.js"
+  }
+]);
